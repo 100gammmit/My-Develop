58ff33ed732a4075468c8e2784be0e1928c1128e
100gammmit <100minha@naver.com>
2022-09-25 21:17:25 +0900

    Update SpringConfig.java

--- spring/src/main/java/hello/spring/SpringConfig.java
+++ spring/src/main/java/hello/spring/SpringConfig.java
@@ -1,13 +1,26 @@
 package hello.spring;
 
+import hello.spring.controller.JpaMemberRepository;
 import hello.spring.repository.MemberRepository;
-import hello.spring.repository.MemoryMemberRepository;
 import hello.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager en;
+    @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager en) {
+        this.dataSource = dataSource;
+        this.en = en;
+    }
+
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +29,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(en);
     }
 }

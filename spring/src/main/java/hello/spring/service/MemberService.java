6514fd6da79840c4f49f5a266668b2355c2b95be
100gammmit <100minha@naver.com>
2022-09-25 17:20:54 +0900

    Create MemberService.java

---
+++ spring/src/main/java/hello/spring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.spring.service;
+
+import hello.spring.domain.Member;
+import hello.spring.repository.MemberRepository;
+import hello.spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     *  회원 가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 아이디 중복 체크 & 예외처리
+     */
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {           //throw를 통한 예외 처리를 임의의 객체로 리턴
+                    throw new IllegalStateException("이미 존재하는 회원");     //이미 Name값이 있을 시 IllegalStateException에러가 발생하므로 이에 따른 예외처리
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById((memberId));
+    }
+}

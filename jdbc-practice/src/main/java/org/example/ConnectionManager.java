e8b792cef8fc93f11de9f60bfd65c81c110db681
100gammmit <100minha@naver.com>
2022-10-07 16:43:31 +0900

    Update ConnectionManager.java

--- jdbc-practice/src/main/java/org/example/ConnectionManager.java
+++ jdbc-practice/src/main/java/org/example/ConnectionManager.java
@@ -3,15 +3,40 @@ package org.example;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionManager {
-    public static DataSource getDataSource() {
+
+    public static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final int MAX_POOL_SIZE = 40;
+
+    private static final DataSource ds;
+
+    static {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:tcp://localhost/~/test");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
-        return hikariDataSource;
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
+        ds = hikariDataSource;
+
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static DataSource getDataSource(){
+        return ds;
     }
 }

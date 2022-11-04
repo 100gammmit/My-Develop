5a7879337c07d9cb639f47e3804a523709349bf3
100gammmit <100minha@naver.com>
2022-11-04 23:10:29 +0900

    Update pt.py

--- pt.py
+++ pt.py
@@ -1,21 +1,46 @@
-from random import randrange
-ad=[]
-bd=[]
-for i in range(1000):
-    ad.append(list())
-    bd.append(list())
-    for j in range(1000):
-        ad[i].append(randrange(1, 100))
-        bd[i].append(randrange(1, 100))
+import sys
 
-with open('a.dat', 'w') as file:
-    for i in range(1000):      
-        for a in ad[i]:
-            file.write(str(a) + ' ')
-        file.write('\n')
-            
-with open('b.dat', 'w') as file:
-    for i in range(1000):      
-        for b in bd[i]:
-            file.write(str(b) + ' ')
-        file.write('\n')        
+def binary_search(li, target):
+    start = 0
+    end = len(li) -1
+
+    while start <= end:
+        middle = (start+end)//2
+        if li[middle]==target:
+            return middle
+        elif li[middle]>target:
+            end = middle - 1
+        else:
+            start = middle + 1
+    return -1
+
+def minpo(target):
+    i = 1
+    while target >= i:
+        i*=2
+    return i//2
+def maxpo(target):
+    i = 1
+    while target > i:
+        i*=2
+    return i
+
+def makebuddy(mmr, target):
+    start = 0
+    while 1:
+        if maxpo(target) in mmr:
+            return mmr.index(target)
+        else:
+            if maxpo(target) > mmr[start]:
+                    start+=1
+                    continue
+            mmr[start] = mmr[start]//2
+            mmr.insert(start, mmr[start])
+
+lst = list(map(int, sys.stdin.readline().split()))
+memory = [minpo(lst.pop(0))]
+idx=0
+for i in lst:
+    print(makebuddy(memory, i))
+    print(memory)
+    idx+=1

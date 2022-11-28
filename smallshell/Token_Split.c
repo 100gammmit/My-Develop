088a2a0e3ef2ec4f1166da938f4ce1bc07f974f4
100gammmit <100minha@naver.com>
2022-11-28 21:18:10 +0900

    Update Token_Split.c

---
+++ smallshell/Token_Split.c
@@ -0,0 +1,15 @@
+#include "main.h"
+int Token_Split(char buff[], char* arg[] ,char delim[]){
+    char* saveptr;
+    char* s;
+    int num = 0;
+    
+    s = strtok_r(buff, delim, &saveptr);
+    while(s) {
+        arg[num++] = s;
+        s = strtok_r(NULL, delim, &saveptr);
+    }
+    
+    arg[num] = (char *)0;
+    return num;
+}

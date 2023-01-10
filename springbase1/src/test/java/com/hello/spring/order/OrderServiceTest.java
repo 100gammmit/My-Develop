b5d05aca0fa1e4c4450ae631b4d58b6e131da97c
100gammmit <100minha@naver.com>
2023-01-10 22:06:07 +0900

    Create OrderServiceTest.java

---
+++ springbase1/src/test/java/com/hello/spring/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package com.hello.spring.order;
+
+import com.hello.spring.member.Grade;
+import com.hello.spring.member.Member;
+import com.hello.spring.member.MemberService;
+import com.hello.spring.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}

9aeb2bb9b58f90996fe352ba3786d2091a16212e
100gammmit <100minha@naver.com>
2023-01-10 22:05:43 +0900

    Create MemberApp.java

---
+++ springbase1/src/main/java/com/hello/spring/MemberApp.java
@@ -0,0 +1,18 @@
+package com.hello.spring;
+
+import com.hello.spring.member.Grade;
+import com.hello.spring.member.Member;
+import com.hello.spring.member.MemberService;
+import com.hello.spring.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}

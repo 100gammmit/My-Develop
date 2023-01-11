91e165171b4e880bc89649c995fc2c92ae3a9e44
100gammmit <100minha@naver.com>
2023-01-11 23:37:27 +0900

    Update MemberServiceTest.java

--- springbase1/src/test/java/com/hello/spring/member/MemberServiceTest.java
+++ springbase1/src/test/java/com/hello/spring/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package com.hello.spring.member;
 
+import com.hello.spring.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {

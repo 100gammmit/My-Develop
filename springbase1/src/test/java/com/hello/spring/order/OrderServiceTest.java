4eb8f0ec2225cef3eed9227570d22ea824ee87b1
100gammmit <100minha@naver.com>
2023-01-11 23:37:29 +0900

    Update OrderServiceTest.java

--- springbase1/src/test/java/com/hello/spring/order/OrderServiceTest.java
+++ springbase1/src/test/java/com/hello/spring/order/OrderServiceTest.java
@@ -1,17 +1,26 @@
 package com.hello.spring.order;
 
+import com.hello.spring.AppConfig;
 import com.hello.spring.member.Grade;
 import com.hello.spring.member.Member;
 import com.hello.spring.member.MemberService;
 import com.hello.spring.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {

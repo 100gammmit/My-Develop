4b002178743b4c12d09acafc6ec84804add91bc6
100gammmit <100minha@naver.com>
2023-01-25 22:32:06 +0900

    Di 적용
    AnnotationConfigApplicationContext를 통하여 MemberService와 OrderService에 대한 Di

--- springbase1/src/main/java/com/hello/spring/OrderApp.java
+++ springbase1/src/main/java/com/hello/spring/OrderApp.java
@@ -7,21 +7,24 @@ import com.hello.spring.member.MemberServiceImpl;
 import com.hello.spring.order.Order;
 import com.hello.spring.order.OrderService;
 import com.hello.spring.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        //MemberService memberService = new MemberServiceImpl();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
 
-        OrderService orderService = appConfig.orderService();
-        //OrderService orderService = new OrderServiceImpl();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "MemberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order.toString());
     }

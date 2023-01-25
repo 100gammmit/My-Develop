32fef6be5c79390af317cb1f8fe02f9fd22171ed
100gammmit <100minha@naver.com>
2023-01-25 22:38:15 +0900

    DI 적용
    AnnotationConfigApplicationContext를 통하여 MemberService에 대한 Di

--- springbase1/src/main/java/com/hello/spring/MemberApp.java
+++ springbase1/src/main/java/com/hello/spring/MemberApp.java
@@ -4,12 +4,18 @@ import com.hello.spring.member.Grade;
 import com.hello.spring.member.Member;
 import com.hello.spring.member.MemberService;
 import com.hello.spring.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        // MemberService memberService = new MemberServiceImpl();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // @Configuration 어노테이션을 적용한 클래스에 대하여 @Bean 이 적용된 객체들을 스프링 컨테이너에 등록함
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // AppConfig 안에서 MemberService 타입의 memberService 함수를 불러들여옴
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 

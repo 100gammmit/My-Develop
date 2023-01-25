6f3e2d4bffec0bd5c60a8ec53f450759b0e8ddb2
100gammmit <100minha@naver.com>
2023-01-25 22:39:55 +0900

    Di 컨테이너 작성
    MemberService, MemberRepository, OrderService, OrderRepository를 빈으로서 생성하고 DI를 수행할 수 있도록 Configuration설정

--- springbase1/src/main/java/com/hello/spring/AppConfig.java
+++ springbase1/src/main/java/com/hello/spring/AppConfig.java
@@ -1,18 +1,34 @@
 package com.hello.spring;
 
+import com.hello.spring.discount.DiscountPolicy;
 import com.hello.spring.discount.FixDiscountPolicy;
+import com.hello.spring.discount.RateDiscountPolicy;
+import com.hello.spring.member.MemberRepository;
 import com.hello.spring.member.MemberService;
 import com.hello.spring.member.MemberServiceImpl;
 import com.hello.spring.member.MemoryMemberRepository;
 import com.hello.spring.order.OrderService;
 import com.hello.spring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration  //프로젝트 설정을 관리하는 클래스임을 스프링에 알리는 어노테이션
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();    // 어떠한 멤버 데이터 관리 정책을 사용할 지 관리하는 코드
+    }
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();     // 어떠한 할인 정책을 적용할지 관리하는 코드
     }
 }

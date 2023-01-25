3a578300400b23d5cea7fde5b036c8dd4fb9cb91
100gammmit <100minha@naver.com>
2023-01-25 22:28:44 +0900

    스프링 상속관계 속에서 빈 조회에 대한 테스트
    부모 타입의 빈을 조회하면 자식타입의 빈까지 같이 딸려온다.

---
+++ springbase1/src/test/java/com/hello/spring/beanfind/ApplicationContextSameBeanFIndTest.java
@@ -0,0 +1,65 @@
+package com.hello.spring.beanfind;
+
+import com.hello.spring.AppConfig;
+import com.hello.spring.member.Member;
+import com.hello.spring.member.MemberRepository;
+import com.hello.spring.member.MemoryMemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanFIndTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
+    void findBeanByTypeDuplicate() {
+        // MemberRepository bean = ac.getBean(MemberRepository.class);
+        assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+    void findBeanByName() {
+        MemberRepository bean = ac.getBean("memberRepository1", MemberRepository.class);
+        assertThat(bean).isInstanceOf(MemoryMemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + "Value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        assertThat(beansOfType.size()).isEqualTo(2);
+    }
+
+
+    /**
+     * 원본 AppConfig 설정 클래스를 건들지 않고
+     * 이 설정 클래스를 토대로 스프링 컨테이너에 빈을 등록하여 테스트
+     */
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
